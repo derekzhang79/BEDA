59fcb32063d5f536f017dde825b45ef0ddfcafe2
Jennifer Kim <jennifer.gahee@gmail.com>
2013-02-17 15:39:59 -0600

    Add initial UIs and Data Manager place holdsr

---
+++ BEDA/DataManager.h
@@ -0,0 +1,13 @@
+//
+//  DataManager.h
+//  BEDA
+//
+//  Created by Jennifer Kim on 2/17/13.
+//  Copyright (c) 2013 Jennifer Soft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataManager : NSObject
+
+@end
---
+++ BEDA/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  BEDA
+//
+//  Created by Jennifer Kim on 2/17/13.
+//  Copyright (c) 2013 Jennifer Soft. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
