c81b5ce5c1d05a4a24ef08ffe6c1e588832701a4
sehoonha <sehoon.ha@gmail.com>
2013-06-16 18:58:55 -0400

    Finally, tabview and tableview become okay

--- BEDA/TableViewController.h
+++ BEDA/TableViewController.h
@@ -1,19 +1,18 @@
 //
-//  DataTableViewController.h
+//  TableViewController.h
 //  BEDA
 //
-//  Created by Sehoon Ha on 6/15/13.
+//  Created by Sehoon Ha on 6/16/13.
 //  Copyright (c) 2013 Jennifer Soft. All rights reserved.
 //
 
-#import "BedaController.h"
+#import <Foundation/Foundation.h>
 #import "SourceTimeData.h"
 
-@interface DataTableViewController : NSObject {
+@interface TableViewController : NSViewController<NSTableViewDelegate, NSTableViewDataSource> {
     IBOutlet NSTableView* tableview;
 }
 
 @property (retain) SourceTimeData* source;
-- (BedaController*) beda;
 
 @end
