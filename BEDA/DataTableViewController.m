cea01d7979473be50cf6ee97ec41ab5c506ebc45
jennifergahee <jennifer.gahee@gmail.com>
2013-06-16 03:53:28 -0500

    addTable values

--- BEDA/DataTableViewController.m
+++ BEDA/DataTableViewController.m
@@ -18,9 +18,44 @@
     SourceTimeData* s = [[[self beda] sources] lastObject];
     [self setSource:s];
     NSLog(@"%s: source name = %@", __PRETTY_FUNCTION__, [[self source] name]);
-
+    
+    NSTableColumn* column = [[NSTableColumn alloc] initWithIdentifier:@"MyView"];
+    [[column headerCell] setStringValue:@"MYHEADER"];
+    [column setWidth:60];
+    [tableview addTableColumn:column];
+    [tableview reloadData];
 
 }
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    NSLog(@"%s = 2", __PRETTY_FUNCTION__);
+    
+    return 4;
+}
+
+
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    NSTextField *result = [tableView makeViewWithIdentifier:@"MyView" owner:self];
+    if (result == nil) {
+        //        result = [[[NSTextField alloc] initWithFrame:...] autorelease];
+        //        result = [[[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 100, 20)] autorelease];
+        result = [[[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 100, 18)] autorelease];
+        result.identifier = @"MyView";
+        [result setBezeled:NO];
+        [result setDrawsBackground:NO];
+        [result setEditable:NO];
+        [result setSelectable:NO];
+    }
+    
+    result.stringValue = @"HEHE";
+    //   [result.textField setStringValue:@"Haha"];
+    
+    // return the result.
+    return result;
+}
+
 
 - (BedaController*) beda {
     return [BedaController getInstance];
